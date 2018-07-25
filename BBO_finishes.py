b3e6fa7a4b78c475d03025c8846bc92746e43d03
Matthew Dallmeyer <d01matt@gmail.com>
2018-07-25 19:34:03 -0400

    Same algorithm, new program structure

--- BBO_finishes.py
+++ BBO_finishes.py
@@ -1,45 +1,41 @@
 # A python program to calculate whether a 1st place
 # tie is possible in the Boneyard Build-Off
-import itertools as it
+from itertools import permutations, product
 import numpy as np
 from collections import Counter
 
-#Number of teams
-n_teams = 4
+# Number of competitions
+n_competitions = 3
 
-#Number of categories
-n_cats = 3
+# Define points used in competition
+points = [1,2,3,4]
 
-#variables
+# variables
 poss_ties = 0
 scenarios = []
-finals = []
 
-perms = it.permutations(range(1,n_teams+1))
-outcomes = it.permutations(perms, n_cats)
+# Tie test
+def isFirstPlaceTied(score):
+    score.sort()
+    return score[-1] == score[-2] #test to see if the two largest are equal
 
-for event in outcomes:
-    finals.append(tuple(sum(x) for x in zip(*event)))
-    tied = [item for item, count in Counter(finals[-1]).most_common(1) if count > 1]
-    if tied:
-        if tied[0] == max(finals[-1]):
-            poss_ties += 1
-            # scenarios.append(finals[-1])
+# generate all possible outcomes of a competition
+competitions = list(permutations(points))
 
-print(poss_ties, poss_ties/24**3)
-# print(finals)
+# combine multiple competitions 
+outcomes = list(product(competitions, repeat=n_competitions))
 
-poss_ties = 0
-permutes = list(it.permutations(range(1,n_teams+1)))
-for race in permutes:
-    for garbage in permutes:
-        for cool in permutes:
-            score = np.array(race)+np.array(garbage)+np.array(cool)
-            tied = [item for item, count in Counter(score).most_common(1) if count > 1]
-            if tied:
-                if tied[0] == score.max():
-                    poss_ties += 1
-                    # scenarios.append(score)
+# sum up the competitions
+final_scores = np.array(outcomes).sum(1).tolist()
 
-print(poss_ties, poss_ties/24**3)
-# print(scenarios)
+# check for ties
+isTied = [isFirstPlaceTied(score) for score in final_scores]
+
+# count ties
+numTied = isTied.count(True)
+
+# calculate percentages
+percentTied = numTied / len(final_scores)
+
+# print result
+print(percentTied)
